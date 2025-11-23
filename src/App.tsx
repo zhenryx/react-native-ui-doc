60a9c9510bba542d4ede876462593543a4fbe97c
czx <>
2025-11-23 17:57:18 +0800

    feat(route):基本菜单跳转-hash

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,21 @@ import './App.css'
 import { Header } from './components/Header'
 import { Aside } from './components/Aside'
 import { MainContent } from './components/MainContent'
+import { useEffect, useState } from 'react'
 function App() {
-
-
+  const [hash,setHash]=useState('#guild/intro')
+  useEffect(()=>{
+    window.addEventListener('hashchange',()=>{
+      setHash(location.hash)
+    }
+    )
+  })
   return (
     <div className='h-screen flex flex-col'>
       <p className='text-[0.8rem] text-center text-(--text-primary)'>If you like my React Native components, give it a star on GitHub! ⭐ </p>
       <Header></Header>
       <div className='flex-1 flex'>
-        <Aside></Aside>
+        <Aside active={hash}></Aside>
         <MainContent></MainContent>
       </div>
     </div>
--- src/components/Aside.tsx
+++ src/components/Aside.tsx
@@ -1,7 +1,50 @@
-export const Aside=()=>{
+import type React from "react"
+const menuItems = [
+  {
+    id: 'guild',
+    title: 'Getting Start',
+    children: [
+      { id: 'Introduction', title: '介绍', path: '#guild/intro' },
+      { id: 'Installation', title: '安装', path: '#guild/start' }
+    ]
+  },
+  {
+    id: 'components',
+    title: 'Components',
+    children: [
+      { id: 'button', title: 'Button 按钮', path: '#components/button' }
+    ]
+  }
+]
+export const Aside: React.FC<{ active: string }> = ({ active }) => {
+  const isActive = (href: string) => {
+    return active === href
+  }
   return (
-    <div className='border-r-1 border-r-gray-400 w-60 h-full'>
-
+    <div className='border-r border-r-gray-400 w-60 h-full'>
+      <ul className="p-4">
+        {menuItems.map(item => (
+          <li key={item.id}>
+            <span className="text-[#999]">{item.title}</span>
+            <ul className="pl-4">
+              {item.children.map(child => (
+                <li key={child.id}>
+                  <a 
+                    href={child.path} 
+                    className={`text-sm block px-2 py-1 rounded ${
+                      isActive(child.path) 
+                        ? 'text-(--text-active) bg-(--bg-active) font-medium' 
+                        : 'text-(--text-primary) hover:bg-(--bg-primary)'
+                    }`}
+                  >
+                    {child.title}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
